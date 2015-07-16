5aacba4f940857abf753846c6ad61f4d2109d3ad
Alexei <bavelski@gmail.com>
2015-07-16 23:40:25 +0200

    ui

--- ui/app/admin/admin.js
+++ ui/app/admin/admin.js
@@ -7,7 +7,7 @@ angular.module('admin', [])
             .state('admin', {
                 url: '/admin',
                 templateUrl: 'admin/admin.tpl.html',
-                controller : 'AdminCtrl'
+                controller: 'AdminCtrl'
             	})
                 	.state('admin.billing', {
                     	url: '/billing',
@@ -15,7 +15,8 @@ angular.module('admin', [])
                 	})
                 	.state('admin.cdr', {
                     	url: '/cdr',
-                    	templateUrl: 'admin/cdr.tpl.html'
+                    	templateUrl: 'admin/cdr.tpl.html',
+					    controller: 'CdrCtrl'
                 	})
                 	.state('admin.provisioning', {
                     	url: '/provisioning',
@@ -24,4 +25,19 @@ angular.module('admin', [])
     })
     .controller('AdminCtrl', function ($scope, $location) {
     	$scope.isActive = function(str){ return $location.path().search(str)>-1; };
+    })
+    .controller('CdrCtrl', function ($scope) {
+        $scope.usageTypes = ['VOICE', 'DATA', 'SMS', 'MMS'];
+        $scope.selectedUsageType = $scope.usageTypes[0];
+
+        $scope.trafficTypes = ['HOME', 'INT', 'ROAM_IN', 'ROAM_OUT'];
+        $scope.selectedTrafficType = $scope.trafficTypes[0];
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
     });
