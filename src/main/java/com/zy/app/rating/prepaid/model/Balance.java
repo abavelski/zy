b74013df801809be84a5b289e8558ffd80728f51
Alexei <bavelski@gmail.com>
2015-09-10 07:03:01 +0200

    balance

---
+++ src/main/java/com/zy/app/rating/prepaid/model/Balance.java
@@ -0,0 +1,12 @@
+package com.zy.app.rating.prepaid.model;
+
+/**
+ * alexei.bavelski@gmail.com
+ * 10/09/15
+ */
+public class Balance {
+    private int id;
+    private int subscriptionId;
+    private double amount;
+    private double reservedAmount;
+}
